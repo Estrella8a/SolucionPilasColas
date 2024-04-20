8dc7b5cb1bcadfc1349e33d95d75402ca66354c5
Edith Ochoa Ramos <edith_112002@hotmail.com>
2024-04-20 08:56:29 -0700

    Agregar Arreglo Pilas y Arreglo Colas

--- PilasColas/Program.cs
+++ PilasColas/Program.cs
@@ -1,41 +1,115 @@
-﻿Arreglos.Pila pila = new Arreglos.Pila(5);
-void Imprimir(string[] elementos)
+﻿using Arreglos;
+
+namespace PilasColas
 {
-    //Console.Clear();
-    Console.WriteLine();
-    foreach (string elemento in elementos)
+    class Program
     {
-        Console.WriteLine(elemento);
+        public static void Main(string[] args)
+        {
+            ArregloPila();
+            ArregloColas();
+        }
+
+
+
+
+        static void ArregloPila()
+        {
+            Console.WriteLine("**************Arreglo Pila**************");
+            Arreglos.Pila pila = new Arreglos.Pila(5);
+            void Imprimir(string[] elementos)
+            {
+                //Console.Clear();
+                Console.WriteLine();
+                foreach (string elemento in elementos)
+                {
+                    Console.WriteLine(elemento);
+                }
+            }
+
+            try
+            {
+                pila.Agregar("Pepe0");
+                pila.Agregar("Pepe1");
+                pila.Agregar("Pepe2");
+                pila.Agregar("Pepe3");
+                pila.Agregar("Pepe4");
+
+                Imprimir(pila.ObtenerArreglo());
+                pila.Eliminar();
+                Imprimir(pila.ObtenerArreglo());
+                pila.Eliminar();
+                Imprimir(pila.ObtenerArreglo());
+                pila.Eliminar();
+                Imprimir(pila.ObtenerArreglo());
+                pila.Eliminar();
+                Imprimir(pila.ObtenerArreglo());
+                pila.Eliminar();
+                Imprimir(pila.ObtenerArreglo());
+                pila.Eliminar();
+                Imprimir(pila.ObtenerArreglo());
+                pila.Eliminar();
+                Imprimir(pila.ObtenerArreglo());
+            }
+            catch (Exception ex)
+            {
+             Console.WriteLine(ex.Message);
+            }
+        }
+
+
+        static void ArregloColas()
+        {
+                Console.WriteLine("**************Arreglo Cola**************");
+                Arreglos.Colas colas = new Arreglos.Colas(5);
+                void Imprimir(string[] elementos)
+                {
+                    //Console.Clear();
+                    Console.WriteLine();
+                    foreach (string elemento in elementos)
+                    {
+                        Console.WriteLine(elemento);
+                    }
+                }
+
+                try
+                {
+                    colas.Agregar("Pepe0");
+                    colas.Agregar("Pepe1");
+                    colas.Agregar("Pepe2");
+                    colas.Agregar("Pepe3");
+                    colas.Agregar("Pepe4");
+
+                    Imprimir(colas.ObtenerArreglo());
+                    colas.Eliminar();
+                    Imprimir(colas.ObtenerArreglo());
+                    colas.Eliminar();
+                    Imprimir(colas.ObtenerArreglo());
+                    colas.Eliminar();
+                    Imprimir(colas.ObtenerArreglo());
+                    colas.Eliminar();
+                    Imprimir(colas.ObtenerArreglo());
+                    colas.Eliminar();
+                    Imprimir(colas.ObtenerArreglo());
+                    colas.Eliminar();
+                    Imprimir(colas.ObtenerArreglo());
+                    colas.Eliminar();
+                    Imprimir(colas.ObtenerArreglo());
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+        }
+       
+
+   
+    
+    
+    
+    
+    
+    
+    
     }
-}
-
-try
-{
-    pila.Agregar("Pepe0");
-    pila.Agregar("Pepe1");
-    pila.Agregar("Pepe2");
-    pila.Agregar("Pepe3");
-    pila.Agregar("Pepe4");
-
-    Imprimir(pila.ObtenerArreglo());
-    pila.Eliminar();
-    Imprimir(pila.ObtenerArreglo());
-    pila.Eliminar();
-    Imprimir(pila.ObtenerArreglo());
-    pila.Eliminar();
-    Imprimir(pila.ObtenerArreglo());
-    pila.Eliminar();
-    Imprimir(pila.ObtenerArreglo());
-    pila.Eliminar();
-    Imprimir(pila.ObtenerArreglo());
-    pila.Eliminar();
-    Imprimir(pila.ObtenerArreglo());
-    pila.Eliminar();
-    Imprimir(pila.ObtenerArreglo());
-
-
-}
-catch (Exception ex)
-{
-    Console.WriteLine(ex.Message);
 }
